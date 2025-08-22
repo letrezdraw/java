baa40b38ded7898e34278178bcc3b4a09e242917
rezoxx <147749303+letrezdraw@users.noreply.github.com>
2025-08-22 10:08:53 +0000

    modified

---
+++ Assi-3/B2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class InvalidNameException extends Exception {
+    public InvalidNameException(String message) { super(message); }
+}
+
+public class B2 {
+    public static void validateName(String name) throws InvalidNameException {
+        if (!name.matches("[a-zA-Z ]+")) throw new InvalidNameException("Name is invalid");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Doctor Name: ");
+        String doctorName = sc.nextLine();
+        try {
+            validateName(doctorName);
+            System.out.println("Doctor Name: " + doctorName);
+        } catch (InvalidNameException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
